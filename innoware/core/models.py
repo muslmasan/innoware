0dc9638135b820b4e0ca55278c83c542c564f9fd
aleerabdallah <juniorab444@gmail.com>
2025-01-11 17:24:06 +0200

    I've delete innoware directory that contains the project

--- innoware/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-
-class UserProfile(AbstractUser):
-
-    image = models.ImageField(blank=True,upload_to='profile_image/')
-    bio = models.TextField(blank=True, null=True)
--- innoware/core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import UserProfile
-
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserProfile
-        fields = ['id','username','bio','image']
